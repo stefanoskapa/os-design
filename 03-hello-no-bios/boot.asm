8efc710c4719a806b90a68e8a9b94cd657d6f62b
Stefanos Kapa <koutsouflakis.stefanos@proton.me>
2026-01-01 23:18:01 +0200

    clear screen

--- 03-hello-no-bios/boot.asm
+++ 03-hello-no-bios/boot.asm
@@ -4,16 +4,25 @@
     cld
     mov ax, 0xb800 ; video memory
     mov es, ax
-
     
-    mov al, 'a'
-    mov ah, 0x0F ; attribute
-    mov cx, ax
-    xor di, di
-    stosw
+    call clear_screen
 
 hang:
     jmp hang
+
+
+
+clear_screen: 
+    mov al, ' ' 
+    mov ah, 0x0F ; attribute
+    mov cx, ax
+    xor di, di
+loop:
+    stosw
+    cmp di, 4000 ; 80 columns * 25 rows
+    jne loop
+    ret
+
 
 
     times 510 - ($-$$) db 0
